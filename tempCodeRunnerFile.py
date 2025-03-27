7e06d801c9440656aa209c758e5e84db1495889b
Cau� Olive Barbosa <52267575817@>
2025-03-27 16:56:17 -0300

    JWT

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-class Círculo(raio,p,a):
-    raio = float(input("Insira o raio:"))
-    p = 2 * 3 * raio
-    a = 3 * (raio**2)
-    print(f"O círculo com raio de {raio}, tem perímetro {p} e área de {a}")
