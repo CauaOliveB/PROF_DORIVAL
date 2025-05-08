34366d54e30e61f7be9032083cc789371bf93845
CauaOliveB <cauaolivebarbosa@gmail.com>
2025-05-08 17:04:51 -0300

    Formativa

--- FORMATIVA/app/models.py
+++ FORMATIVA/app/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 
+class professor(models.Model):
+    NI = models.PositiveIntegerField(primary_key=True)
+    nome = models.CharField(max_length=50)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=11)
+    data_de_nascimento = models.DateField()
