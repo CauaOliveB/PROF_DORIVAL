4ce504498cf3a6962a443bcc93c4cea4c8e966f9
CauaOliveB <cauaolivebarbosa@gmail.com>
2025-05-15 01:00:35 -0300

    Concluded

--- FORMATIVA/app/urls.py
+++ FORMATIVA/app/urls.py
@@ -2,5 +2,12 @@ from django .urls import path
 from . import views 
 
 urlpatterns = [
-   
+   path('professor/', view=views.ProfessorListCreateAPIView.as_view(), name='Professor-GET-POST'),
+   path('professor/<int:pk>', view=views.ProfessorRetrieveUpdateDestroyAPIView.as_view(), name='Professor-PUT-DELETE-GET'),
+
+   path('disciplina/', view=views.DisciplinaListCreateAPIView.as_view(), name='Disciplina-GET-POST'),
+   path('disciplina/<int:pk>/', view=views.DisciplinaRetrieveUpdateDestroyAPIView.as_view(), name='Disciplina-PUT-DELETE-GET'),
+
+   path('disciplina/', view=views.ReservaDeClasseListCreateAPIView.as_view(), name='ReservaDeClasse-GET-POST'),
+   path('disciplina/<int:pk>/', view=views.ReservaDeClasseRetrieveUpdateDestroyAPIView.as_view(), name='ReservaDeClasse-PUT-DELETE-GET'),
 ]
--- FORMATIVA/app/views.py
+++ FORMATIVA/app/views.py
@@ -2,12 +2,46 @@ from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpda
 from .serializers import ProfessorSerializer, DisciplinaSerializer, ReservaDeClasseSerializer
 from .models import Professor, Disciplina, ReservaDeClasse
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response 
 from .permissions import IsManager
-from rest_framework import status
- 
 
-class DisciplinaListCreateAPIView(ListAPIView):
+ 
+"""""
+
+*--------------------------------*
+|                                |
+|          Professores           |
+|                                |
+*--------------------------------*
+
+"""""
+
+class ProfessorListCreateAPIView(ListCreateAPIView):
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsAuthenticated()]
+        return [IsManager()]
+    
+class ProfessorRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Professor.objects.all()
+    serializer_class = ProfessorSerializer
+    
+    def get_permissions(self):
+        return[IsManager()]
+    
+"""""
+
+*--------------------------------*
+|                                |
+|          Disciplina            |
+|                                |
+*--------------------------------*
+
+"""""
+
+class DisciplinaListCreateAPIView(ListCreateAPIView):
     queryset = Disciplina.objects.all()
     serializer_class = DisciplinaSerializer
 
@@ -16,20 +50,37 @@ class DisciplinaListCreateAPIView(ListAPIView):
             return [IsAuthenticated()]
         return [IsManager]
 
-@api_view(['GET'])
-def ListarProfessoresView(request):
-    queryset = Professor.objects.all()
-    serializer = ProfessorSerializer (queryset, many=True)
-    return Response(serializer.data,status=status.HTTP_200_OK)
+class DisciplinaRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = Disciplina.objects.all()
+    serializer_class = DisciplinaSerializer
+    
+    def get_permissions(self):
+        return[IsManager()]
+    
 
-@api_view(['POST'])
-def CadastrarProfessoresView(request):
-    if request.method == 'POST':
-        serializer = ProfessorSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+"""""
 
-# @api_view(['PUT'])
-# def atualizer_cadastro_professores(request):
+*--------------------------------*
+|                                |
+|            Reserva             |
+|                                |
+*--------------------------------*
+
+"""""
+
+class ReservaDeClasseListCreateAPIView(ListCreateAPIView):
+    queryset = ReservaDeClasse.objects.all()
+    serializer_class = ReservaDeClasseSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [IsAuthenticated()]
+        return [IsManager]
+
+class ReservaDeClasseRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = ReservaDeClasse.objects.all()
+    serializer_class = ReservaDeClasseSerializer
+    
+    def get_permissions(self):
+        return[IsManager()]
+    
